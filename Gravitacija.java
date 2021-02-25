53241c02965b2638567835d32872b0ce84cb5304
A-glitch-bat <ag3507@student.uni-lj.si>
2021-02-25 12:02:43 +0100

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,15 @@
-public class Gravitacija {
-    public static void main(String[] args) {
-        System.out.println("Ois je zakon!");
+import java.util.*;
 
-        System.out.println("Ois je zakon!");System.out.println("Ois je zakon!");
+public class Gravitacija {
+	
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double v = sc.nextInt();
+		double CM = 3.9857 * Math.pow(10, 14);
+		double r = 6.371 * Math.pow(10, 6);
+		
+		double imenovalec = (r + v) * (r + v);
+		double gravPospesek = CM/imenovalec;
     }
 }
